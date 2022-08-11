eed366575da11738c09e2dccf175986f5864ed10
Jonatan <natanmagalrodri@gmail.com>
2022-08-11 16:16:42 -0300

    Calculadora

---
+++ script.js
@@ -0,0 +1,27 @@
+let numero1 = prompt('Digite o primeiro número')
+let operacao = prompt('Digite uma operação (+, -, *, /)')
+let numero2 = prompt('Digite o segundo número')
+let resultado = 0
+console.log(operacao)
+console.log(numero1)
+console.log(numero2)
+
+if(operacao == "+" || operacao == "soma"){
+    resultado = Number(numero1) + Number(numero2)
+    alert(resultado)
+}
+
+if(operacao == "-" || operacao == "subtração"){
+    resultado = Number(numero1) - Number(numero2)
+    alert(resultado)
+}
+
+if(operacao == "*" || operacao == "multiplicação"){
+    resultado = Number(numero1) * Number(numero2)
+    alert(resultado)
+}
+
+if(operacao == "/" || operacao == "divisão"){
+    resultado = Number(numero1) / Number(numero2)
+    alert(resultado)
+}
